1efc1f3e542377ce537ec7e49577f39500984de1
infactum <infactum@gmail.com>
2018-05-19 23:45:56 -0700

    appveyor linux

--- install_prereq.bat
+++ install_prereq.bat
@@ -1,2 +1,7 @@
-xcopy ports\. "%vcpkg_path%\ports\" /E
-vcpkg install openssl:%TARGET_PLATFORM%-windows-static zlib:%TARGET_PLATFORM%-windows-static icu:%TARGET_PLATFORM%-windows-static tdjson:%TARGET_PLATFORM%-windows-static
+@echo off
+
+set _VCPKG_PATH=%~1
+set _TARGET_PLATFORM=%~2
+
+xcopy ports\. "%_VCPKG_PATH%\ports\" /E
+vcpkg install openssl:%_TARGET_PLATFORM%-windows-static zlib:%_TARGET_PLATFORM%-windows-static icu:%_TARGET_PLATFORM%-windows-static tdjson:%_TARGET_PLATFORM%-windows-static

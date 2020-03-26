03edc83e4654370e77f88e67c0611140ff5f8a1b
infactum <infactum@gmail.com>
2020-03-27 01:23:24 +0500

    update install_prereq.bat

--- install_prereq.bat
+++ install_prereq.bat
@@ -8,4 +8,5 @@ xcopy tools\vcpkg\. "%_VCPKG_PATH%\" /E
 vcpkg install ^
     openssl:%_TARGET_TRIPLET% ^
     zlib:%_TARGET_TRIPLET% ^
-    tdlib:%_TARGET_TRIPLET%
+    tdlib:%_TARGET_TRIPLET% ^
+    icu:%_TARGET_TRIPLET%
